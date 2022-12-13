32e855d6827aedf0cea2dfba6b0d3dc24ef5b589
Vlad Gritsavka <griczavka2363@gmail.com>
2022-12-13 15:11:06 +0300

    changed port for amvera

--- server/index.js
+++ server/index.js
@@ -13,11 +13,11 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = 3001;
+const PORT = 80;
 const app = express();
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use('/', cityRoutes);
 app.use('/filters', filtersRoutes);
 app.use('/users', usersRoutes);
